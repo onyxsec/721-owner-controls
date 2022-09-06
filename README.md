97ba5e2f7e7482b282696d06355e76a11798fae2
onyxsec <etherealsec@gmail.com>
2022-09-06 12:01:59 -0700

    add goerli testnet env and contract verification

--- README.md
+++ README.md
@@ -7,6 +7,8 @@
 npm install -g truffle
 npm install --save-dev @openzeppelin/test-helpers
 npm install @openzeppelin/contracts
+npm install @truffle/hdwallet-provider
+npm install truffle-plugin-verify
 ```
 
 ### Try it out
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,11 @@
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const secrets = require('./secrets')
+
+//const secrets = reqire('secrets.js')
+//import { INFURA } from 'secrets.js'
+//import { HDWalletProvider } from '@truffle/hdwallet-provider'
+//const HDWalletProvider = require("@truffle/hdwallet-provider")
+
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -82,13 +90,15 @@ module.exports = {
     //
     // Useful for deploying to a public network.
     // Note: It's important to wrap the provider as a function to ensure truffle uses a new provider every time.
-    // goerli: {
-    //   provider: () => new HDWalletProvider(MNEMONIC, `https://goerli.infura.io/v3/${PROJECT_ID}`),
-    //   network_id: 5,       // Goerli's id
-    //   confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
-    //   timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-    //   skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    // },
+    goerli: {
+      provider: () => new HDWalletProvider(secrets.METAMASK_MNEMONIC_PHRASE, secrets.INFURA),
+      network_id: 5,
+      gas: 8500000,
+      gasPrice: 1000000000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
@@ -117,6 +127,14 @@ module.exports = {
       // }
     }
   },
+
+  plugins: ['truffle-plugin-verify'],
+
+  api_keys: {
+    etherscan: secrets.ETHERSCAN_KEY
+  },
+
+
 
   // Truffle DB is currently disabled by default; to enable it, change enabled:
   // false to enabled: true. The default storage location can also be
